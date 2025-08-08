07493be63b6bcadab70ad537357f079d3066b665
tenzinyounten <tenzin.aimit2014@gmail.com>
2025-08-08 11:48:21 +0530

    feat: enhance main.py with LLM integration and update dependencies

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 import asyncio
-from dotenv import load_dotenv
 import os
+
+from dotenv import load_dotenv
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langchain_openai import ChatOpenAI
+from langgraph.prebuilt import create_react_agent
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
 
 # Load environment variables from .env file
 load_dotenv()
+
+llm = ChatOpenAI()
+stdio_server_params = StdioServerParameters(
+    command= "python",
+    args= ["/Users/tenzinyounten/Documents/AI/mcp-servers/langchain-mcp/langchain-mcp-adapter/servers/math_server.py"],
+    env= {
+        "OPENAI_API_KEY": os.getenv("OPENAI_API_KEY")
+    }
+)
 
 # Check if the environment variable is loaded
 api_key = os.getenv("OPENAI_API_KEY")
 if api_key:
     print(f"OPENAI_API_KEY loaded successfully: {api_key}")
-else:
-    print("OPENAI_API_KEY not found. Please check your .env file.")
-    print("Make sure you have a .env file in your project root with:")
-    print("OPENAI_API_KEY=your_actual_api_key_here")
+
 
 async def main():
     print("Hello from langchain-mcp-adapter!")

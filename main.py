d943a62789d7983ed2887223fbfc5fec791a3f45
tenzinyounten <tenzin.aimit2014@gmail.com>
2025-08-08 14:42:37 +0530

    feat: implement MCP server connection and LLM agent invocation in main.py

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import asyncio
 import os
 
 from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage
 from langchain_mcp_adapters.tools import load_mcp_tools
 from langchain_openai import ChatOpenAI
+from langgraph.checkpoint.serde.types import C
 from langgraph.prebuilt import create_react_agent
 from mcp import ClientSession, StdioServerParameters
 from mcp.client.stdio import stdio_client
@@ -27,7 +29,17 @@ if api_key:
 
 
 async def main():
-    print("Hello from langchain-mcp-adapter!")
+    async with stdio_client(stdio_server_params) as (read, write):
+        async with ClientSession(read_stream=read, write_stream=write) as session:
+            await session.initialize()
+            print("Connected to MCP server")
+            tools = await load_mcp_tools(session)
+            print(tools)
+
+            agent = create_react_agent(llm, tools)
+            result = await agent.ainvoke({"messages": [HumanMessage(content="What is 43 / 2 ?")]})
+            print(result["messages"][-1].content)
+            
     
 if __name__ == "__main__":
     asyncio.run(main())
